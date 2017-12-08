d7c5c47d52c227b565142f21ffe6fdc46a813294
schoeu <schoeu89@gmail.com>
2017-12-08 22:44:03 +0800

    delete useless code.

--- actions/push.js
+++ actions/push.js
@@ -7,24 +7,12 @@
 const path = require('path');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-
-//const exec = require('child_process').exec;
+const transScript = 'transit.sh';
 
 // 推送操作
 module.exports = async function (...args) {
-    let shell = path.join(__dirname, '../scripts', './test.sh');
+    let shell = path.join(__dirname, '../scripts', transScript);
 
     // 更新
     return await exec('sh ' + shell + ' ' + args.join(' '));
 }
-
-// 推送操作
-// module.exports = sync function (...args) {
-//     let shell = path.join(__dirname, '../scripts', './test.sh');
-
-//     // 更新
-//     exec('sh ' + shell, args, (error, stdout, stderr) => {
-//         console.log(`stdout: ${stdout}`);
-//         console.log(`stderr: ${stderr}`);
-//     });
-// }
